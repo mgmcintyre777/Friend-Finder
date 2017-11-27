6c61e29c63886914db6e016500104e94aa0fa42a
Matt McIntyre <mgmcintyre777@gmail.com>
2017-11-27 16:27:41 -0500

    init

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
